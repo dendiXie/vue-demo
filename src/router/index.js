322ab0a7dad258bd62ff5048d482ddc2c838ae31
xiewenhao-001 <xiewenhao-001@cpic.com.cn>
2020-11-07 14:57:07 +0800

    跳转

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import HelloVue from '@/components/test/HelloVue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'HelloVue',
+      component: HelloVue
     }
   ]
 })
